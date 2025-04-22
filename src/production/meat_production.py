23c15147971f4f42418cd0b3137c3728e406e27d
Clon54 <clon5454@outlook.com>
2025-04-21 21:29:13 -0500

    Implemente un singleton en la clase MeatProduction

--- src/production/meat_production.py
+++ src/production/meat_production.py
@@ -1,8 +1,16 @@
-from production import Production, singleton
+from production import Production
 
 
-@singleton
 class MeatProduction(Production):
 
+    _instance = None
+
     def __init__(self, cant: int = 0):
-        super().__init__(cant, "Carne de res")
+        if not hasattr(self, "_initialized"):
+            super().__init__(cant, "Carne de res")
+            self._in_initialized = True
+
+    def __new__(cls, cant: int = 0):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
