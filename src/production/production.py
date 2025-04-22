42de9e27bb5719dec688c3f2c8be7cfe2e017b8f
Clon54 <clon5454@outlook.com>
2025-04-21 21:28:42 -0500

    Elimine el decorador singleton.

--- src/production/production.py
+++ src/production/production.py
@@ -1,15 +1,3 @@
-# from product.product import Product
-
-
-def singleton(cls):
-    instances = {}
-
-    def get_instance(*args, **kwargs):
-        if cls not in instances:
-            instances[cls] = cls(*args, **kwargs)
-        return instances[cls]
-
-    return get_instance
 
 
 class Production:
