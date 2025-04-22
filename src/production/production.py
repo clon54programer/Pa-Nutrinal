5be5741a79e29cd141c874c44f38ffa12b11424f
Clon54 <clon5454@outlook.com>
2025-04-21 21:10:20 -0500

    agregue el setter de cant.

--- src/production/production.py
+++ src/production/production.py
@@ -18,6 +18,9 @@ class Production:
         self.__cant = cant
         self.__name_product = name_product
 
+    def set_cant(self, cant: int):
+        self.__cant = cant
+
     def get_cant(self) -> int:
         return self.__cant
 
