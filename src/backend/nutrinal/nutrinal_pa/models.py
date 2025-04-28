150ce36f632d33490e6ac671bbcd5474392c509b
Clon54 <clon5454@outlook.com>
2025-04-28 16:46:32 -0500

    Agregue la documentacion de los modelos Client y order..

--- src/backend/nutrinal/nutrinal_pa/models.py
+++ src/backend/nutrinal/nutrinal_pa/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    """
+    Es una abstracion de un cliente de nutrinal,
+    que solo guarda su nombre y sus informacion de contacto.
+
+    TODO: Integrar los modelos de Django a la clase.
+
+    Atributos
+    ---------
+
+    __name: str
+        Es una cadena que almacena un nombre de una persona
+        natural o de una organizacion.
+    
+    __email: str
+        Es una cadena que almacena el correo de la persona
+        o organizacion.
+    
+    __phone_number: str
+        Es una cadena que almacena un numero de telefono.
+        NOTE: Esta cadena solo puede tener numeros en ella.
+
+    __cant_order: int
+        Es un numero entero que almacena el numero de compras que 
+        ha echo el cliente.
+
+    __order: llave foranea
+        Es una llave foranea que enlaza su historial de pedidos
+    """
+
+
+
+
+class Order(models.Model):
+    """
+    Es una representacion de un pedido.
+
+    Atributos
+    --------
+
+    __cant: int
+        Es un numero de entero que almacena la cantidad del 
+        producto que se debe enviar
+
+    __id_product: int
+        Es un numero entero que almacena el codigo que identica
+        al producto que se compra.
+
+    __shipping_destination: str
+        Es una cadena que almacena la direccion a la cual se debe
+        enviar el pedido.
+
+    __seller: LLave foranea
+        Sera una llave foranea que hara referencia al vendedor
+        que atendio al cliente
+
+    __client: llave foranea
+        Es una llave forano que hace referencia al cliente.
+        NOTE: Cuando se haga el pedido se debe verificar que
+        el cliente este en la base de datos.
+    """
