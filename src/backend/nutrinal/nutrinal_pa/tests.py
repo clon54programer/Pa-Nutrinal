4691bf58a5bc0486a998ba282a55ee1f372a76b0
Clon54 <clon5454@outlook.com>
2025-04-28 22:56:18 -0500

    intento de implementar los test.

--- src/backend/nutrinal/nutrinal_pa/tests.py
+++ src/backend/nutrinal/nutrinal_pa/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase, Client
 from django.http import Http404
+import requests
 
+from .JsonReponse import ReponseJson,StatusResponse
+
+class ViewCreateSellerLogin(TestCase):
+
+    def create_seller(self):
+        route = "http://127.0.0.1:8000/nutrinal_pa/admin/create_seller_login"
+
+        seller_data = {"name": "Juan",
+                       "id": "1234567",
+                       "username": "seller_1",
+                       "password": "12345678"
+                       }
+
+        data = ReponseJson(200,StatusResponse.VALID,seller_data)
+
+        response = requests.post(route, data=data)
+
+        error = response.json()
+
+        print(error)
+
+        self.assertEquals(True,True)
