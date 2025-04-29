38064588a5fc5b533da21b0fc4944ba4d773bc82
Clon54 <clon5454@outlook.com>
2025-04-28 20:29:20 -0500

    Implemente la vista login.

--- src/backend/nutrinal/nutrinal_pa/views.py
+++ src/backend/nutrinal/nutrinal_pa/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Bienvenido a nutrinal")
+
+
+def login(request,type_user: str):
+
+    if type_user == "admin":
+        return HttpResponse(f"Bienvenido: {type_user}")
+    elif type_user == "client":
+        return HttpResponse(f"Bienvenido: {type_user}")
+    elif type_user == "seller":
+        return HttpResponse(f"Bienvenido: {type_user}")
+
+
+    return Http404("Tipo de usuario invalido")
