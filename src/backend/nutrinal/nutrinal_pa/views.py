07b4c8ea163536cc627fd0d5b310d1c96dc6c107
Clon54 <clon5454@outlook.com>
2025-04-28 21:32:48 -0500

    Implemente la respuesta en formato json en la vista login.

--- src/backend/nutrinal/nutrinal_pa/views.py
+++ src/backend/nutrinal/nutrinal_pa/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+from .JsonReponse import ResponseJson,StatusResponse
 
 # Create your views here.
 
@@ -18,13 +19,16 @@ def login(request,type_user: str):
     # data, que se almacenara la informacion
 
     if type_user == "admin":
-        return HttpResponse(f"Bienvenido: {type_user}")
+        #return HttpResponse(f"Bienvenido: {type_user}")
+        return ResponseJson(StatusResponse.VALID,{"data": f"Bienvenido {type_user}"}).get_reponse()
     elif type_user == "client":
-        return HttpResponse(f"Bienvenido: {type_user}")
+        #return HttpResponse(f"Bienvenido: {type_user}")
+        return ResponseJson(StatusResponse.VALID,{"data": f"Bienvenido {type_user}"}).get_reponse()
     elif type_user == "seller":
-        return HttpResponse(f"Bienvenido: {type_user}")
+        #return HttpResponse(f"Bienvenido: {type_user}")
+        return ResponseJson(StatusResponse.VALID,{"data": f"Bienvenido {type_user}"}).get_reponse()
     #else:
     #    raise Http404("El tipo de usuario es invalido")
 
 
-    return JsonResponse(data="s")
+    return ResponseJson(StatusResponse.INVALID,{"data": "No existe ese usuario"}).get_reponse()
