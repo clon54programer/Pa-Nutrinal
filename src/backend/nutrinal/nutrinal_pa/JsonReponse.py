bbba79680ed4d57666c0ee892ea71b539ba20039
Clon54 <clon5454@outlook.com>
2025-04-28 21:25:13 -0500

    Implemente la clase ReponseJson.

--- src/backend/nutrinal/nutrinal_pa/JsonReponse.py
+++ src/backend/nutrinal/nutrinal_pa/JsonReponse.py
@@ -1,7 +1,11 @@
-
+from django.http import JsonResponse
 
 class ResponseJson:
     
 
     def __init__(self,status: str, data: dict[str,str]) -> None:
+        self.json_reponse = JsonResponse(data={"status":status,"data":data})
         pass
+
+    def get_reponse(self):
+        return self.json_reponse
