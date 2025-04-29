e438ce10d6ea526c086ac4c277a8fa333aa1b01d
Clon54 <clon5454@outlook.com>
2025-04-28 20:37:35 -0500

    Crree unos test.

--- src/backend/nutrinal/nutrinal_pa/tests.py
+++ src/backend/nutrinal/nutrinal_pa/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.http import Http404
 
-# Create your tests here.
+class LoginViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()  
+
+    def test_admin_user(self):
+        response = self.client.get('/login/admin/')  
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Bienvenido: admin", response.content)
+
+    def test_client_user(self):
+        response = self.client.get('/login/client/')  
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Bienvenido: client", response.content)
+
+    def test_seller_user(self):
+        response = self.client.get('/login/seller/')  
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Bienvenido: seller", response.content)
+
+    def test_invalid_user(self):
+        with self.assertRaises(Http404):  # Prueba para un tipo de usuario inválido
+            self.client.get('/login/unknown/')
