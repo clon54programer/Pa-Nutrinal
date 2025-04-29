0a1983962e0d6cc74d9b8e24b2a30cf1b4817f60
Clon54 <clon5454@outlook.com>
2025-04-28 21:11:36 -0500

    Elimine los test.

--- src/backend/nutrinal/nutrinal_pa/tests.py
+++ src/backend/nutrinal/nutrinal_pa/tests.py
@@ -1,26 +1,3 @@
 from django.test import TestCase, Client
 from django.http import Http404
 
-class LoginViewTest(TestCase):
-   
-    def setUp(self):
-        self.client = Client()  # Cliente de prueba para simular solicitudes HTTP
-
-    def test_admin_user(self):
-        response = self.client.get('/login/admin/')
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Bienvenido: admin")  # Corrige la verificación de contenido
-
-    def test_client_user(self):
-        response = self.client.get('/login/client/')
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Bienvenido: client")  # Corrige la verificación de contenido
-
-    def test_seller_user(self):
-        response = self.client.get('/login/seller/')
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Bienvenido: seller")  # Corrige la verificación de contenido
-
-    def test_invalid_user(self):
-        response = self.client.get('/login/unknown/')
-        self.assertEqual(response.status_code, 404)
