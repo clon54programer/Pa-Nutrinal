7289fd81a7b5e42771168f457cc04719fabab6c8
Clon54 <clon5454@outlook.com>
2025-05-03 21:00:35 -0500

    Cree el primer test

--- src/backend/nutrinal/nutrinal_pa/tests.py
+++ src/backend/nutrinal/nutrinal_pa/tests.py
@@ -6,13 +6,21 @@ from .JsonReponse import ReponseJson, StatusResponse
 
 class TestViewLogin(TestCase):
 
-    def test_valid_reponse():
+    def test_valid_reponse(self):
 
         routes_valid = [
             "http://127.0.0.1:8000/nutrinal_pa/login/client",
             "http://127.0.0.1:8000/nutrinal_pa/login/seller",
             "http://127.0.0.1:8000/nutrinal_pa/login/admin"
         ]
+
+        for iter in routes_valid:
+            r = requests.get(iter)
+
+            self.assertEqual(200, r.status_code,
+                             "El codigo http de respuesta es diferente a 200")
+
+            print(r.text)
 
         route_invalid = [
             "http://127.0.0.1:8000/nutrinal_pa/login/body",
