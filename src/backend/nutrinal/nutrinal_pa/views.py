8ca5a56fd29de798088754860374fb48f46d4e69
Clon54 <clon5454@outlook.com>
2025-05-03 20:43:12 -0500

    Cambie el valor de retorno al detectar un error en la vista Lign.

--- src/backend/nutrinal/nutrinal_pa/views.py
+++ src/backend/nutrinal/nutrinal_pa/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse,HttpRequest
-from .JsonReponse import StatusResponse,ReponseJson,ReponseJsonError
+from django.http import HttpResponse, JsonResponse, HttpRequest
+from .JsonReponse import StatusResponse, ReponseJson, ReponseJsonError
 
 from . import ValidRequest
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Bienvenido a nutrinal")
 
 
-def login(request,type_user: str):
-
-    
+def login(request, type_user: str):
 
     # Estructura de la respuesta en json
     # Un campo llamado, status,
@@ -21,24 +20,25 @@ def login(request,type_user: str):
     # data, que se almacenara la informacion
 
     if type_user == "admin":
-        
-        return ReponseJson(200,StatusResponse.VALID,f"Bienvenido: {type_user}")
+
+        return ReponseJson(200, StatusResponse.VALID, {"data": f"Bienvenido: {type_user}"})
     elif type_user == "client":
-        
-        return ReponseJson(200,StatusResponse.VALID,f"Bienvenido: {type_user}")
+
+        return ReponseJson(200, StatusResponse.VALID, {"data": f"Bienvenido: {type_user}"})
     elif type_user == "seller":
-        
-        return ReponseJson(200,StatusResponse.VALID,f"Bienvenido: {type_user}")
-    #else:
+
+        return ReponseJson(200, StatusResponse.VALID, {"data": f"Bienvenido: {type_user}"})
+    # else:
     #    raise Http404("El tipo de usuario es invalido")
 
+    # return ReponseJson(404, StatusResponse.INVALID, f"Tipo de usuario invalido")
+    return ReponseJsonError("Tipo de usuario invalido", "La ruta no contiene un tipo de usuario iqual a seller, admin o client")
 
-    return ReponseJson(404,StatusResponse.INVALID,f"Tipo de usuario invalido")
 
 def create_seller_login(request: HttpRequest):
     """
     Recibi mediante de un metodo post del panel de administracion de
-    un admin, la informacion de una entidad de tipo seller y 
+    un admin, la informacion de una entidad de tipo seller y
     seller_login.
 
     Esta funcion recibe los siguiente datos:
@@ -46,12 +46,9 @@ def create_seller_login(request: HttpRequest):
         - id: str
         - username: str
         - password: str
-    NOTE: data_joined, debe ser asignado al momento de recibir la 
+    NOTE: data_joined, debe ser asignado al momento de recibir la
         solicitud.
     """
-
-    
-
 
     if request.method == "POST":
         name_seller = request.POST.get("name")
@@ -59,21 +56,14 @@ def create_seller_login(request: HttpRequest):
         username_seller = request.POST.get("username")
         password_seller = request.POST.get("password")
 
-        data = [name_seller,id_seller,username_seller,password_seller]
+        data = [name_seller, id_seller, username_seller, password_seller]
 
         for iter in data:
             if ValidRequest.is_valid(iter) != True:
                 message = f"el parametro: {type(iter).__name__}"
-                return ReponseJsonError("Falto enviar un dato",details=message)
-
-            
+                return ReponseJsonError("Falto enviar un dato", details=message)
 
     else:
         return HttpResponse("Error")
-
-
-
-
-
 
     return HttpResponse("Create")
