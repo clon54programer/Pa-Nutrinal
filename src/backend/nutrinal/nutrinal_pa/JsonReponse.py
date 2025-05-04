9f3618ca536833819b744acb7c3164d4e6e53c7f
Clon54 <clon5454@outlook.com>
2025-05-03 20:38:47 -0500

    Cambie el nombre de un parametro para evitar colision de nombres con una variable interna de la funcion.

--- src/backend/nutrinal/nutrinal_pa/JsonReponse.py
+++ src/backend/nutrinal/nutrinal_pa/JsonReponse.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from enum import Enum
-#from django.core.serializers import serialize
+# from django.core.serializers import serialize
+
 
 class StatusResponse(Enum):
     """
@@ -10,29 +11,27 @@ class StatusResponse(Enum):
     VALID = 0
     INVALID = -1
 
-
-    def __str__(self) ->  str: 
+    def __str__(self) -> str:
         if self.value == 0:
             return "valid"
 
         return "invalid"
-        
 
 
-
-def ReponseJson(code_http: int, status: StatusResponse, data) -> JsonResponse:
+def ReponseJson(code_http: int, status: StatusResponse, json_body: dict[str, str | int]) -> JsonResponse:
     data = {
-        "status":status.__str__(),
+        "status": status.__str__(),
         "code": code_http.__str__(),
-        "data": data
+        "data": json_body
     }
 
     return JsonResponse(data=data)
 
-def ReponseJsonError(error: str, details: str,code_http: int = 400):
+
+def ReponseJsonError(error: str, details: str, code_http: int = 400):
     data = {
-    "error": error,
-    "details": details
+        "error": error,
+        "details": details
     }
 
-    return JsonResponse(data=data,status=code_http)
+    return JsonResponse(data=data, status=code_http)
